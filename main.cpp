2d371edc4d83bef55f89172cf453198059a8b393
awpsun <harshitraj2005@gmail.com>
2024-08-01 21:47:09 +0530

    First C++ Program made

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/* #include <iostream>
+int main(){
+    auto result = (10 <=> 20) > 0;
+    std::cout << result << std::endl;
+} */
+#include <iostream>
+
+int main() {
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    std::cout << "NAME" << std::endl;
+    return 0;
+}
+/* #include <iostream>
+
+int main() {
+    std::cout << "Number 1" << std::endl;
+    std::cout << "Number 2" <<std::endl;
+    std::cout << "Number 3" <<std::endl;
+    return 0;
+}
+ */
